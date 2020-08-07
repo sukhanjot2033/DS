8fe22d8c73a49c66dce091b5728c26a3311af464
sukhanjot singh <68323427+sukhanjot2033@users.noreply.github.com>
2020-08-07 13:03:02 +0530

    Add files via upload

---
+++ Detect_A_Loop.java
@@ -0,0 +1,63 @@
+package CircularLinklist;
+import java.util.*;
+
+public class Detect_A_Loop {
+	  
+    static Node head; 
+  
+    static class Node { 
+        int data; 
+        Node next; 
+        Node(int d) 
+        { 
+            data = d; 
+            next = null; 
+        } 
+    } 
+  
+    static public void push(int new_data) 
+    { 
+        Node new_node = new Node(new_data); 
+      
+        new_node.next = head; 
+  
+        head = new_node; 
+    } 
+
+    static boolean detectLoop(Node h) 
+    { 
+        HashSet<Node> s = new HashSet<Node>(); 
+        while (h != null) { 
+      
+            if (s.contains(h)) 
+                return true; 
+  
+     
+            s.add(h); 
+  
+            h = h.next; 
+        } 
+  
+        return false; 
+    } 
+  
+  
+    public static void main(String[] args) 
+    { 
+    	Detect_A_Loop llist = new Detect_A_Loop(); 
+  
+        llist.push(20); 
+        llist.push(4); 
+        llist.push(15); 
+        llist.push(10); 
+  
+
+        llist.head.next.next.next.next = llist.head; 
+  
+        if (detectLoop(head)) 
+            System.out.println("Loop found"); 
+        else
+            System.out.println("No Loop"); 
+    } 
+} 
+  
