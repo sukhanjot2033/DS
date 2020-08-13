305d57318eec178550974129dbeeea332298c057
sukhanjot singh <68323427+sukhanjot2033@users.noreply.github.com>
2020-08-13 16:00:43 +0530

    Evalution from postfix to infix

---
+++ Evl_postfix.java
@@ -0,0 +1,41 @@
+package INFIX;
+import java.util.*;
+
+public class Evl_postfix {
+	static boolean isOperand(char x) 
+	{ 
+	    return (x >= 'a' && x <= 'z') ||  (x >= 'A' && x <= 'Z'); 
+	} 
+	  
+	static String getInfix(String exp) 
+	{ 
+	    Stack<String> s = new Stack<String>(); 
+	  
+	    for (int i = 0; i < exp.length(); i++) 
+	    { 
+	  
+	        if (isOperand(exp.charAt(i))) 
+	        { 
+	        s.push(exp.charAt(i) + ""); 
+	        } 
+	        else
+	        { 
+	            String op1 = s.peek(); 
+	            s.pop(); 
+	            String op2 = s.peek(); 
+	            s.pop(); 
+	            s.push("(" + op2 + exp.charAt(i) + 
+	                    op1 + ")"); 
+	        } 
+	    } 
+	  
+	
+	    return s.peek(); 
+	} 
+	  
+	public static void main(String args[]) 
+	{ 
+	    String exp = "ab*c+"; 
+	    System.out.println( getInfix(exp)); 
+	} 
+	} 
